d687a35ee378f56063605a668d54be415f077df7
Aron Smit <aronsmit@hotmail.com>
2019-04-03 14:23:36 +0200

    add search function for google api, searches nearby bars

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -1,5 +1,21 @@
 const https = require("follow-redirects").https;
 
-const placeDetails = function () {
+const placeDetails = function() {
     this.place = [];
 };
+
+const latitude = 52.361778;
+const longitude = 4.907370;
+const radius = 500;
+
+function placeSearch(latitude, longitude, radius) {
+    https.request(
+        {
+            host: "maps.googleapis.com",
+            path: "/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" + radius + "&type=bar&key=AIzaSyBDu1B08NDVs8DlY5nRQcJfVouuyyVcpNw",
+            method: "GET"
+        }
+    );
+}
+
+placeSearch(latitude, longitude, radius);
