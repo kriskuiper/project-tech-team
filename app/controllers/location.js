248b16fddfae1932fe3824de22516f7d0d5772e5
Aron Smit <aronsmit@hotmail.com>
2019-04-03 22:45:10 +0200

    write data from google api to array

--- app/controllers/location.js
+++ app/controllers/location.js
@@ -1,21 +1,54 @@
 const https = require("follow-redirects").https;
+require("dotenv").config();
 
 const placeDetails = function() {
-    this.place = [];
+    this.places = [];
 };
 
+//Declare loction information
+const googleApiKey = process.env.GOOGLE_API;
 const latitude = 52.361778;
 const longitude = 4.907370;
 const radius = 500;
 
+
+//Find places within the specified radius, based on the coordinates provided by the getLocation function.
 function placeSearch(latitude, longitude, radius) {
-    https.request(
-        {
-            host: "maps.googleapis.com",
-            path: "/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" + radius + "&type=bar&key=AIzaSyBDu1B08NDVs8DlY5nRQcJfVouuyyVcpNw",
-            method: "GET"
-        }
-    );
+    https.request({
+        host: "maps.googleapis.com",
+        path: "/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=" + radius + "&type=bar&key=" + googleApiKey,
+        method: "GET"
+    },
+        PlaceResult).end();
 }
+
+function PlaceResult(response) { 
+    let p;
+    let data = "";
+    let sdata = "";
+    const PD = new placeDetails();
+
+    response.on("data", (chunk) => {
+        data += chunk;
+    });
+    response.on("end", () => {
+        sdata = JSON.parse(data);
+        if (sdata.status === "OK") {
+            console.log("Status: " + sdata.status);
+            console.log("Results: " + sdata.results.length);
+            for (p = 0; p < sdata.results.length; p++) {
+                PD.places.push(sdata.results[p]);
+            }
+            for (let i = 0; i < sdata.results.length; i++) {
+                console.log("----------------------------------------------");
+                console.log("Name: " + PD.places[i].name);
+                console.log("adress: " + PD.places[i].vicinity);
+            }
+        } else {
+            console.log(sdata.status);
+        }
+    });
+}
+
 
 placeSearch(latitude, longitude, radius);
