0a593cab120fc9413132ff06ff211f08e8316d84
Rick <rickmichiel@icloud.com>
2019-04-08 15:30:49 +0200

    Add password route

--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -4,7 +4,18 @@ const User = require("../models/User");
 async function setPassword(req, res, next) {
     try {
         const password = req.body;
-        await User.update({'username': req.session.user.username}, { 'password': password });
+        console.log(req.session.user.username);
+        // await User.update({'username': req.session.user.username}, { 'password': password });
+
+
+        User.findOne({ 'username': req.session.user.username }, function (err, user) {
+          if (err) {
+            console.log("error");
+          }
+          user.password = password;
+          user.save(callback);
+        })
+
         res.redirect("/");
 
     } catch(error) {
