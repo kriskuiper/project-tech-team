d06858a1ff0a280b9d6242b2d965f58cf0d20fd3
Rick <rickmichiel@icloud.com>
2019-04-08 15:49:19 +0200

    Add untappd connect with redirect to set Password route.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,33 +1,42 @@
 const User = require("../models/User");
 
 async function login(req, res, next) {
-    try {
-        const {username, password} = req.body;
+  try {
+    const {
+      username,
+      password
+    } = req.body;
 
-        await User.findOne({ 'username': username.toLowerCase() }, function (err, data) {
-          if (err) return handleError(err);
+    await User.findOne({
+      'username': username.toLowerCase()
+    }, function(err, data) {
+      if (err) return handleError(err);
 
-          if (data.password === password) {
+      if (data.password === password) {
 
-            req.session.user = {
-              firstName: data.firstName,
-              lastName: data.lastName,
-              profilePicture: data.profilePicture,
-              gender: data.gender,
-              age: data.age,
-              likedpersons: data.likedpersons,
-              beers: data.beers
-            };
-            res.render("home", {user: req.session.user});
-          } else {
-            const error = "Username or password incorrect";
-            res.status(403).render("login", {error: error});
-          }
-        })
+        req.session.user = {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          profilePicture: data.profilePicture,
+          gender: data.gender,
+          age: data.age,
+          likedpersons: data.likedpersons,
+          beers: data.beers
+        };
+        res.render("home", {
+          user: req.session.user
+        });
+      } else {
+        const error = "Username or password incorrect";
+        res.status(403).render("login", {
+          error: error
+        });
+      }
+    })
 
-    } catch(error) {
-        next(error);
-    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = login;
--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -3,8 +3,7 @@ const User = require("../models/User");
 
 async function setPassword(req, res, next) {
     try {
-        console.log(req.session.user.username);
-
+      
         await User.updateOne({'username': req.session.user.username}, { 'password': req.body.password });
 
         res.redirect("/");
