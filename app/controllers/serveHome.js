06041548fabf2d72de9b23abd22969d6c36a9ef9
Rick <rickmichiel@icloud.com>
2019-04-08 14:25:54 +0200

    Update untappd auth

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -4,7 +4,7 @@ function serveHome(req, res, user) {
     if (!req.session.user) {
         res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
     } else {
-        res.status(200).render("feed", user: req.session.user);
+        res.status(200).render("home", user: req.session.user);
     }
 }
 
