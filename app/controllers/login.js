e2ac38a64ee8a5a3ec4138114a005384c7c8b75e
Rick <rickmichiel@icloud.com>
2019-04-08 14:40:22 +0200

    Update untappd auth

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,20 +4,16 @@ async function login(req, res, next) {
     try {
         const users = await User.find();
         const {username, password} = req.body;
-        
-        // Source for this loop: Kaan Cenik
-        for (let i = 0; i < users.length; i++) {
-            if (username.toLowerCase() === users[i].username && password === users[i].password) {
-                req.session.user = {
-                    firstName: users[i].firstName,
-                    lastName: users[i].lastName,
-                    bike: users[i].bike
-                };
-                res.redirect("/my-feed");
-            } else {
-                const error = "Username or password incorrect";
-                res.status(403).render("login", {error: error});
-            }
+
+        if (username.toLowerCase() === users[i].username && password === users[i].password) {
+            req.session.user = {
+                firstName: users[i].firstName,
+                lastName: users[i].lastName
+            };
+            res.redirect("/");
+        } else {
+            const error = "Username or password incorrect";
+            res.status(403).render("login", {error: error});
         }
     } catch(error) {
         next(error);
