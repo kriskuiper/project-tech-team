2c80b64ce62e32f95cd056ed5ad52070be8614fa
Rick <rickmichiel@icloud.com>
2019-04-09 17:29:03 +0200

    Fix search beer issue

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -39,7 +39,6 @@ function beerSearch(event) {
       let Beers = data.response.beers.items
       document.getElementById("beer__search").classList.add("active");
       for (let i = 0; i < Beers.length; i++) {
-        // console.log('<li onClick="addBeer(' + Beers[i].beer.bid + ')">' + Beers[i].beer.beer_name + '</li>');
         document.getElementById('beer_results').innerHTML +=
 
           '<form action="/add-beer" method="post"><img src="' + Beers[i].beer.beer_label + '"><input class="beer__name invisible" name="beerImg" value="' + Beers[i].beer.beer_label + '"><input class="beer__bid invisible" name="beerBid" value="' + Beers[i].beer.bid + '"><input class="beer__name invisible" name="beerName" value="' + Beers[i].beer.beer_name + '"><h3>' + Beers[i].beer.beer_name + '</h3><input class="beer__description invisible" name="beerDescription" value="' + Beers[i].beer.beer_description + '"><p>' + Beers[i].beer.beer_description + '</p><input class="brewery_name invisible" name="brewery" value="' + Beers[i].brewery.brewery_name + '"><p>' + Beers[i].brewery.brewery_name + '</p><button type="submit" name="button">Add</button></form>'
@@ -48,4 +47,8 @@ function beerSearch(event) {
     })
     .catch(error => console.error('Error:', error))
   event.preventDefault()
+}
+
+function beerSearchDelete(event) {
+  document.getElementById('beer_results') = ''
 }
