cdd10e6ce074d0ddebccdea7775f5cfec6bccef4
Rick <rickmichiel@icloud.com>
2019-04-09 12:11:10 +0200

    Add new variale sets to user databse

--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -2,64 +2,59 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 
 async function setPassword(req, res, next) {
-    try {
+  try {
 
-      await User.updateMany({
+    await User.updateMany({
+      'username': req.session.user.username
+    }, {
+      'password': req.body.password,
+      'age': req.body.age,
+      'gender': req.body.gender,
+      'prefered_age': {
+        min: req.body.age_min,
+        max: req.body.age_max
+      },
+      'prefered_gender': req.body.prefered_gender
+    });
+
+    pushVariables()
+
+    function pushVariables() {
+
+      req.session.user['age' = req.body.age,
+      'gender' = req.body.gender,
+      'prefered_gender' = req.body.prefered_gender]
+    }
+
+    updateUser()
+
+    function updateUser() {
+      User.find({
         'username': req.session.user.username
-      }, {
-        'password': req.body.password,
-        'age': req.body.age,
-        'gender': req.body.gender,
-        'prefered_age': {
-          min: req.body.age_min,
-          max: req.body.age_max
-        },
-        'prefered_gender': req.body.prefered_gender
-      });
+      }).exec(function(err, user) {
+        if (err) return handleError(err);
 
-      pushVariables()
-
-      function pushVariables() {
-        req.session.user.push({
-          age: req.body.age,
-          gender: req.body.gender,
-          prefered_age: {
-            min: req.body.age_min,
-            max: req.body.age_max
-          },
-          prefered_gender: req.body.prefered_gender
-        });
-      }
-
-      updateUser()
-
-      function updateUser() {
-        User.find({
-          'username': req.session.user.username
-        }).exec(function(err, user) {
-            if (err) return handleError(err);
-
-            if (user.length > 0) {
-              user.updateMany({}, {
-                  $set: {
-                    age: req.body.age,
-                    gender: req.body.gender,
-                    prefered_age: {
-                      min: req.body.age_min,
-                      max: req.body.age_max
-                    },
-                    prefered_gender: req.body.prefered_gender
-                  }
-                })
-              }
-            })
-          }
-
-            res.redirect("/");
-
-          } catch (error) {
-            next(error);
-          }
+        if (user.length > 0) {
+          user.updateMany({}, {
+            $set: {
+              age: req.body.age,
+              gender: req.body.gender,
+              prefered_age: {
+                min: req.body.age_min,
+                max: req.body.age_max
+              },
+              prefered_gender: req.body.prefered_gender
+            }
+          })
         }
+      })
+    }
 
-        module.exports = setPassword;
+    res.redirect("/");
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = setPassword;
