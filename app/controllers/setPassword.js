776e6c86b18ab4f284c16e6c29e9c7215e6b5e63
Rick <rickmichiel@icloud.com>
2019-04-09 11:50:08 +0200

    Add new variale sets to user databse

--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -2,32 +2,59 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 
 async function setPassword(req, res, next) {
-  try {
+    try {
 
-    await User.updateMany({
-      'username': req.session.user.username
-    }, {
-      'password': req.body.password,
-      'age': req.body.age,
-      'gender': req.body.gender,
-      'prefered_age': {min: req.body.age_min,
-                       max: req.body.age_max},
-      'prefered_gender': req.body.prefered_gender
-    });
+      await User.updateMany({
+        'username': req.session.user.username
+      }, {
+        'password': req.body.password,
+        'age': req.body.age,
+        'gender': req.body.gender,
+        'prefered_age': {
+          min: req.body.age_min,
+          max: req.body.age_max
+        },
+        'prefered_gender': req.body.prefered_gender
+      });
 
-    req.session.user.push({
-      age: req.body.age,
-      gender: req.body.gender,
-      prefered_age: {min: req.body.age_min,
-                    max: req.body.age_max},
-      prefered_gender: req.body.prefered_gender
-    });
+      function pushVariables() {
+        req.session.user.push({
+          age: req.body.age,
+          gender: req.body.gender,
+          prefered_age: {
+            min: req.body.age_min,
+            max: req.body.age_max
+          },
+          prefered_gender: req.body.prefered_gender
+        });
+      }
 
-    res.redirect("/");
+      function updateUser() {
+        User.find({
+          'username': req.session.user.username
+        }).exec(function(err, user) {
+            if (err) return handleError(err);
 
-  } catch (error) {
-    next(error);
-  }
-}
+            if (user.length > 0) {
+              user.updateMany({}, {
+                  $set: {
+                    age: req.body.age,
+                    gender: req.body.gender,
+                    prefered_age: {
+                      min: req.body.age_min,
+                      max: req.body.age_max
+                    },
+                    prefered_gender: req.body.prefered_gender
+                  }
+                }
+              })
+            }
 
-module.exports = setPassword;
+            res.redirect("/");
+
+          } catch (error) {
+            next(error);
+          }
+        }
+
+        module.exports = setPassword;
