9308c5dcedc6e62d920248164fe37ec5db81354e
Rick <rickmichiel@icloud.com>
2019-04-09 12:23:25 +0200

    Add new variale sets to user databse

--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -21,35 +21,39 @@ async function setPassword(req, res, next) {
 
     function pushVariables() {
 
-      req.session.user['age'] = req.body.age;
+      req.session.user.age = req.body.age;
+      req.session.user.gender = req.body.gender;
+      req.session.user.prefered_age.min = req.body.age_min;
+      req.session.user.prefered_age.max = req.body.age_max;
+      req.session.user.prefered_gender = req.body.prefered_gender;
 
       console.log(req.session.user);
     }
 
-    // updateUser()
-    //
-    // function updateUser() {
-    //   User.find({
-    //     'username': req.session.user.username
-    //   }).exec(function(err, user) {
-    //     if (err) return handleError(err);
-    //     console.log(user.length);
-    //
-    //     if (user.length > 0) {
-    //       user.updateMany({}, {
-    //         $set: {
-    //           age: req.body.age,
-    //           gender: req.body.gender,
-    //           prefered_age: {
-    //             min: req.body.age_min,
-    //             max: req.body.age_max
-    //           },
-    //           prefered_gender: req.body.prefered_gender
-    //         }, {upsert: true}
-    //       })
-    //     }
-    //   })
-    // }
+    updateUser()
+
+    function updateUser() {
+      User.find({
+        'username': req.session.user.username
+      }).exec(function(err, user) {
+        if (err) return handleError(err);
+        console.log(user.length);
+
+        if (user.length > 0) {
+          user.update({}, {
+            $set: {
+              age: req.body.age,
+              gender: req.body.gender,
+              prefered_age: {
+                min: req.body.age_min,
+                max: req.body.age_max
+              },
+              prefered_gender: req.body.prefered_gender
+            }, {upsert: true}
+          })
+        }
+      })
+    }
 
     res.redirect("/");
 
