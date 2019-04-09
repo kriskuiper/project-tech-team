d81f4f1b2a15815c857e2b54a85551c63a822aef
Mark Schutte <mark.schutte@hva.nl>
2019-04-09 23:30:13 +0200

    Removed liked persons from user page

--- app/controllers/match.js
+++ app/controllers/match.js
@@ -1,36 +1,11 @@
+const _array = require("lodash/array");
+
 //Require list
 const User = require("../models/User");
 //This wil be the list of the liked persons of the logged-in user.
-
+//This function will add users to the loggedin array users
 async function match(req, res, next) {
     //Gets the personid that will be added to the list
-    try {
-        const {personid} = req.query;
-        const users = await User.find();
-        for (let i = 0; i < users.length; i++) {
-            //Checks if the users is already been liked
-            if (users[i].id === req.query.likedId) {
-                console.log("This user is already been liked");
-                return;
-            } else {
-                console.log("The user has been liked!");
-                // TODO: add real data instead of users[0]
-                users[0].likedpersons.push(personid);
-                users[0].save();
-            }
-            //Saves the info about the user and sends it to matches.ejs
-            res.status(200).render("users", {users: users});
-        }
-    }
-    catch(error) {
-        next(error);
-    }
-}
-module.exports = match;
-
-//This function will add users to the loggedin array users
-async function match1(req, res, next) {
-    //gets the personid that will be added to the list
     try {
         const {personid} = req.query;
         const users = await User.find();
@@ -38,12 +13,21 @@ async function match1(req, res, next) {
             // TODO: add real data instead of users[0]
             users[0].likedpersons.push(personid);
             users[0].save();
+            //remove user from userpage
         }
-        // Saves the info about the user and sends it to matches.ejs
-        res.status(200).render("users", {users: users});
+
+        const extractIds = users.map(user => user.id);
+
+        for (let i = 0; i < users[0].likedpersons.length; i++) {
+            _array.pull(extractIds, users[0].likedpersons[i]);
+        }
+
+        console.log(extractIds);
+        //Saves the info about the user and sends it to matches.ejs
+        res.status(200).render("users", {users: extractIds});
     }
     catch(error) {
         next(error);
     }
 }
-module.exports = match1;
+module.exports = match;
