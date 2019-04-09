01ba21135ad9b4e388ff273f5cf1cc167c047dd9
Rick <rickmichiel@icloud.com>
2019-04-09 11:16:57 +0200

    Add Untappd login, add beers on homescreen

--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 async function setPassword(req, res, next) {
     try {
 
-        await User.update({'username': req.session.user.username}, { 'password': req.body.password, 'age': req.body.age, 'gender': req.body.gender });
+        await User.updateMany({'username': req.session.user.username}, { 'password': req.body.password, 'age': req.body.age, 'gender': req.body.gender });
 
         res.redirect("/");
 
--- app/controllers/untappdAuth.js
+++ app/controllers/untappdAuth.js
@@ -11,39 +11,47 @@ function untappdAuth(req, res) {
   const CODE = req.query.code
   let REDIRECT_URL = 'https://untappdtest.herokuapp.com/untappd-authentication'
 
-  fetch('https://untappd.com/oauth/authorize/?client_id=' + process.env.CLIENTID + '&client_secret=' + process.env.CLIENTSECRET + '&response_type=code&redirect_url=' + REDIRECT_URL + '&code=' + CODE , {
-    method: 'GET'
-    })
-  .then(response => response.json())
-  .then(function(data) {
-    console.log(data);
-
-
-    const ACCESS_TOKEN = data.response.access_token
-    fetch('https://api.untappd.com/v4/user/info?access_token=' + ACCESS_TOKEN, {
+  fetch('https://untappd.com/oauth/authorize/?client_id=' + process.env.CLIENTID + '&client_secret=' + process.env.CLIENTSECRET + '&response_type=code&redirect_url=' + REDIRECT_URL + '&code=' + CODE, {
       method: 'GET'
-      })
+    })
     .then(response => response.json())
     .then(function(data) {
-      const beersArray = [];
+      console.log(data);
 
-      for (var i = 0; i < data.response.user.recent_brews.count; i++) {
-        let beer_bid = data.response.user.recent_brews.items[i].beer.bid;
-        let beer_name = data.response.user.recent_brews.items[i].beer.beer_name;
-        let beer_label = data.response.user.recent_brews.items[i].beer.beer_label;
-        let beer_description = data.response.user.recent_brews.items[i].beer.beer_description;
-        let beer_brewery = data.response.user.recent_brews.items[i].brewery.brewery_name;
-        objectBeer = {beer: {bid: beer_bid, name: beer_name, img: beer_label, description: beer_description, brewery: beer_brewery}};
 
-        beersArray.push(objectBeer);
-      }
+      const ACCESS_TOKEN = data.response.access_token
+      fetch('https://api.untappd.com/v4/user/info?access_token=' + ACCESS_TOKEN, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(function(data) {
+          const beersArray = [];
 
-      setSession();
+          for (var i = 0; i < data.response.user.recent_brews.count; i++) {
+            let beer_bid = data.response.user.recent_brews.items[i].beer.bid;
+            let beer_name = data.response.user.recent_brews.items[i].beer.beer_name;
+            let beer_label = data.response.user.recent_brews.items[i].beer.beer_label;
+            let beer_description = data.response.user.recent_brews.items[i].beer.beer_description;
+            let beer_brewery = data.response.user.recent_brews.items[i].brewery.brewery_name;
+            objectBeer = {
+              beer: {
+                bid: beer_bid,
+                name: beer_name,
+                img: beer_label,
+                description: beer_description,
+                brewery: beer_brewery
+              }
+            };
 
-      function setSession(error) {
-          if (error) {
+            beersArray.push(objectBeer);
+          }
+
+          setSession();
+
+          function setSession(error) {
+            if (error) {
               next(error);
-          } else {
+            } else {
               req.session.user = {
                 username: data.response.user.user_name,
                 firstName: data.response.user.first_name,
@@ -51,20 +59,22 @@ function untappdAuth(req, res) {
                 profilePicture: data.response.user.user_avatar_hd,
                 beers: beersArray
               };
+            }
           }
-      }
 
-        User.find({ 'username': req.session.user.username }).exec( function (err, user) {
-          if (err) return handleError(err);
+          User.find({
+            'username': req.session.user.username
+          }).exec(function(err, user) {
+            if (err) return handleError(err);
 
-          console.log(req.session.user.username);
-          console.log(user.length);
+            console.log(req.session.user.username);
+            console.log(user.length);
 
-          if (user.length > 0) {
-            console.log("Name exisits");
-            res.redirect("/")
-          } else {
-            const newUser = new User({
+            if (user.length > 0) {
+              console.log("Name exisits");
+              res.redirect("/")
+            } else {
+              const newUser = new User({
                 _id: new mongoose.Types.ObjectId(),
                 username: req.session.user.username,
                 password: null,
@@ -72,20 +82,20 @@ function untappdAuth(req, res) {
                 lastName: req.session.user.lastName,
                 profilePicture: req.session.user.profilePicture,
                 beers: beersArray
-            });
+              });
 
-            User.create(newUser);
-            if (req.session.password == null) {
-        res.redirect("/set-password")
-      } else {
-        res.redirect("/")
-      }
-    }
-        });
+              User.create(newUser);
+              if (req.session.password == null) {
+                res.redirect("/set-password")
+              } else {
+                res.redirect("/")
+              }
+            }
+          });
+        })
+        .catch(error => console.error('Error:', error))
     })
     .catch(error => console.error('Error:', error))
-  })
-  .catch(error => console.error('Error:', error))
 }
 
 module.exports = untappdAuth;
