5112fd8384cfb36893fd401f8e90f959bc7c32af
Rick <rickmichiel@icloud.com>
2019-04-10 13:55:12 +0200

    Fix

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -2,24 +2,34 @@ const path = require("path");
 const fetch = require("node-fetch");
 
 async function serveHome(req, res, user) {
-    if (!req.session.user) {
-        res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
+  if (!req.session.user) {
+    res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
+  } else {
+    const beerValue = req.body.beerName;
+
+    if (beerValue) {
+
+      const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue +
+        '&client_id=' + process.env.CLIENTID +
+        '&client_secret=' + process.env.CLIENTSECRET
+      );
+      const beerObjects = await beerResults.json();
+
+      res.status(200).render("home", {
+        user: req.session.user,
+        beerResults: beerResults.response
+      });
+
     } else {
-        const beerValue = req.body.beerName;
 
-        if (beerValue) {
+      beerResults = '';
 
-          const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue
-                                    + '&client_id=' + process.env.CLIENTID
-                                    + '&client_secret=' + process.env.CLIENTSECRET
-                              );
-          const beerObjects = await beerResults.json();
-        }
-
-        beerResults = '';
-
-        res.status(200).render("home", {user: req.session.user, beerResults: beerResults.response.beers});
+      res.status(200).render("home", {
+        user: req.session.user,
+        beerResults: beerResults
+      });
     }
+  }
 }
 
 module.exports = serveHome;
