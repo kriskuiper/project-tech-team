62d937fe0da2e430b1b5a989779bcac9d8420968
Rick <rickmichiel@icloud.com>
2019-04-10 15:43:10 +0200

    Fix securty issue

--- app/controllers/renderLogin.js
+++ app/controllers/renderLogin.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 function renderLogin(req, res,) {
-  const authLink = 'https://untappd.com/oauth/authenticate/?client_id=' + CLIENTID + '&response_type=code&redirect_url=' + REDIRECT_URL;
+  const authLink = 'https://untappd.com/oauth/authenticate/?client_id=' + process.env.CLIENTID + '&response_type=code&redirect_url=' + process.env.REDIRECT_URL;
 
     res.status(200).render("login", {error: false, authLink: authLink});
 }
