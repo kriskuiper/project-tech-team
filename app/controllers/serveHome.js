2b2866a3e2f4222b61617cfc1b7610ab3d21ce76
Rick <rickmichiel@icloud.com>
2019-04-10 14:36:47 +0200

    Fix

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -15,7 +15,7 @@ async function serveHome(req, res, user) {
       );
       const beerObjects = await beerResults.json();
 
-      console.log(beerObjects.response.beers.items[1]);
+      console.log(beerObjects.response.beers.items[1].beer.beer_name);
 
       // console.log(beerObjects.beers.items[1].beer.beer_name);
 
