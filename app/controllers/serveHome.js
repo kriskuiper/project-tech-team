3d31f032701eba755f2b24f3b5ed5ac1fbe1dfc7
Rick <rickmichiel@icloud.com>
2019-04-10 13:38:18 +0200

    Fix

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -5,10 +5,11 @@ async function serveHome(req, res, user) {
     if (!req.session.user) {
         res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
     } else {
-        const { beerValue } = req.body.beerName;
-                console.log(beerValue);
+        const { beerValue } = req.body;
 
         if (beerValue) {
+
+          console.log(beerValue);
           const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue
                                     + '&client_id=' + process.env.CLIENTID
                                     + '&client_secret=' + process.env.CLIENTSECRET
