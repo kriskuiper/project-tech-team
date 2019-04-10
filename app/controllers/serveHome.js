12f6072af668de3d6db29383a90f08c1614fc9bb
Rick <rickmichiel@icloud.com>
2019-04-10 13:10:11 +0200

    fix env in main.js

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -1,11 +1,24 @@
 const path = require("path");
 const fetch = require("node-fetch");
 
-function serveHome(req, res, user) {
+async function serveHome(req, res, user) {
     if (!req.session.user) {
         res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
     } else {
-        res.status(200).render("home", {user: req.session.user, fetch: fetch});
+        const { beerValue } = req.body;
+
+        if (beerValue) {
+          const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue
+                                    + '&client_id=' + process.env.CLIENTID
+                                    + '&client_secret=' + process.env.CLIENTSECRET
+                              );
+          const beerObjects = await beerResults.json();
+          console.log(beerObjects);
+        }
+
+        beerResults = '';
+
+        res.status(200).render("home", {user: req.session.user, beerResults: beerResults});
     }
 }
 
--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 // Cutting the mustard technique
 if ("querySelector" in document) {
   document.body.classList.add("js-enabled");
@@ -28,28 +25,4 @@ function showPosts(entries) {
       entryClass.add("is--visible");
     }
   }
-}
-
-function beerSearch(event) {
-
-  fetch('https://api.untappd.com/v4/search/beer?q=' + document.getElementById("searchName").value + '&client_id=' + dotenv.CLIENTID + '&client_secret=' + dotenv.CLIENTSECRET, {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(function(data) {
-      let Beers = data.response.beers.items
-      document.getElementById("beer__search").classList.add("active");
-      for (let i = 0; i < Beers.length; i++) {
-        document.getElementById('beer_results').innerHTML +=
-
-          '<form action="/add-beer" method="post"><img src="' + Beers[i].beer.beer_label + '"><input class="beer__name invisible" name="beerImg" value="' + Beers[i].beer.beer_label + '"><input class="beer__bid invisible" name="beerBid" value="' + Beers[i].beer.bid + '"><input class="beer__name invisible" name="beerName" value="' + Beers[i].beer.beer_name + '"><h3>' + Beers[i].beer.beer_name + '</h3><input class="beer__description invisible" name="beerDescription" value="' + Beers[i].beer.beer_description + '"><p>' + Beers[i].beer.beer_description + '</p><input class="brewery_name invisible" name="brewery" value="' + Beers[i].brewery.brewery_name + '"><p>' + Beers[i].brewery.brewery_name + '</p><button type="submit" name="button">Add</button></form>'
-
-      }
-    })
-    .catch(error => console.error('Error:', error))
-  event.preventDefault()
-}
-
-function beerSearchDelete(event) {
-  document.getElementById('beer_results') = ''
 }
