d6577a0ffd4fbdc18e984fe046e193718e9c6964
Rick <rickmichiel@icloud.com>
2019-04-10 14:53:46 +0200

    Fix

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -6,7 +6,6 @@ async function serveHome(req, res, user) {
     res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
   } else {
     const beerValue = req.body.beerName;
-    console.log(req.body + " is search value");
     console.log(req.body.beerName + " is search value");
 
     if (beerValue) {
