ba3cc5455b3325b6abf2991d8354d40805e830ec
Rick <rickmichiel@icloud.com>
2019-04-10 14:45:53 +0200

    Fix

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -6,11 +6,11 @@ async function serveHome(req, res, user) {
     res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
   } else {
     const beerValue = req.body.beerName;
-    console.log(beerValue.value + " is search value");
+    console.log(beerValue + " is search value");
 
     if (beerValue) {
 
-      const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue.value +
+      const beerResults = await fetch('https://api.untappd.com/v4/search/beer?q=' + beerValue +
         '&client_id=' + process.env.CLIENTID +
         '&client_secret=' + process.env.CLIENTSECRET
       );
