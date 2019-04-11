102ce5d033dae023a663452b9ca80c18c8c9a489
Chazzers <chazz567@gmail.com>
2019-04-11 11:12:24 +0200

    fix binding name

--- app/controllers/allFilter.js
+++ app/controllers/allFilter.js
@@ -1,9 +1,9 @@
-const User = require("../models/User.js");
+const filterUsers = require("../models/User.js");
 
 async function allFilter(req, res, next) {
   try {
     let { gender, min, max } = req.query;
-    const users = await User.find();
+    const users = await filterUsers.find();
 
     const filterAll = users.filter(user => user.age >= min && user.age <= max && user.gender === gender);
 
