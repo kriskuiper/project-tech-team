b98a5f519711f0256bf809f11c4d8292e5166cef
Rick <rickmichiel@icloud.com>
2019-04-11 21:44:52 +0200

    Add index.html styling

--- app/controllers/addBeer.js
+++ app/controllers/addBeer.js
@@ -22,7 +22,7 @@ async function addBeer(req, res, next) {
 
     beersArray.push(objectBeer);
 
-    User.updateMany({
+    await User.updateMany({
       'username': req.session.user.username
     }, {
       'beers': beersArray
