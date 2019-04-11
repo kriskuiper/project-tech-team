5db5804cb462edc137707cc7d3c8125dc587916c
Kris <kriskuiper74@gmail.com>
2019-04-11 17:50:04 +0200

    Trying to fix....

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 function serveHome(req, res) {
+  console.log("HALKFHKHKAHEALHEK");
   if (!req.session.user) {
     res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
   } else {
