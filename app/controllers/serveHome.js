0aea0985033f187857a30787603d9a744ad14e9f
Kris <kriskuiper74@gmail.com>
2019-04-11 17:20:30 +0200

    Fix setPassword

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
-async function serveHome(req, res) {
-  console.log("==== ======");
+function serveHome(req, res) {
   if (!req.session.user) {
+    console.log("No user specified");
     res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
   } else {
+    console.log("We've got a user");
     const beerResults = "";
       res.status(200).render("home", {
         user: req.session.user,
--- app/controllers/setPassword.js
+++ app/controllers/setPassword.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const User = require("../models/User");
 
 async function setPassword(req, res, next) {
@@ -10,22 +9,13 @@ async function setPassword(req, res, next) {
       'password': req.body.password,
       'age': req.body.age,
       'gender': req.body.gender,
-      'preferred_age': {
-        min: req.body.age_min,
-        max: req.body.age_max
-      },
-      'preferred_gender': req.body.preferred_gender
     });
 
     pushVariables()
 
     function pushVariables() {
-
       req.session.user.age = req.body.age;
       req.session.user.gender = req.body.gender;
-      req.session.user.preferred_age.min = req.body.age_min;
-      req.session.user.preferred_age.max = req.body.age_max;
-      req.session.user.preferred_gender = req.body.preferred_gender;
     }
 
     res.redirect("/");
