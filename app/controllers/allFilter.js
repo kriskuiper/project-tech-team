47040def2ac872a1db433bf80d9a6a3d7db5f275
Chazzers <chazz567@gmail.com>
2019-04-11 10:19:03 +0200

    fix filter feature

--- app/controllers/allFilter.js
+++ app/controllers/allFilter.js
@@ -4,11 +4,10 @@ async function allFilter(req, res, next) {
   try {
     let { gender, min, max } = req.query;
     const users = await User.find();
+
     const filterAll = users.filter(user => user.age >= min && user.age <= max && user.gender === gender);
 
-
     res.render("users", { users: filterAll });
-
   } catch (error) {
     next(error);
   }
