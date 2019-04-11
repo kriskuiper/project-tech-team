56ef06b032375e0181d03e4c4a31190b3719c84e
Mark Schutte <mark.schutte@hva.nl>
2019-04-11 10:18:11 +0200

    Removed the liked person from array

--- app/controllers/match.js
+++ app/controllers/match.js
@@ -8,24 +8,44 @@ async function match(req, res, next) {
     //Gets the personid that will be added to the list
     try {
         const {personid} = req.query;
+        //Get all info from database
         const users = await User.find();
+
         if (personid) {
             // TODO: add real data instead of users[0]
             users[0].likedpersons.push(personid);
             users[0].save();
             //remove user from userpage
         }
-
+        
         const extractIds = users.map(user => user.id);
 
         for (let i = 0; i < users[0].likedpersons.length; i++) {
             _array.pull(extractIds, users[0].likedpersons[i]);
         }
 
-        console.log(extractIds);
-        //Saves the info about the user and sends it to matches.ejs
-        res.status(200).render("users", {users: extractIds});
-    }
+        //extractIds is list of users that aren't liked
+        //console.log(extractIds);
+        //console.log(users);
+
+        const unlikedUsers = [];
+
+        for (let i = 0; i < extractIds.length; i++) {
+            unlikedUsers.push(User.findById(extractIds[i], (err, res) => {
+                if (err) {
+                    next(err);
+                } else {
+                    return res;
+                }
+            }));
+        }
+
+        // When the for-loop is finished the system will show the matches
+        await Promise.all(unlikedUsers)
+            .then(results => {
+                res.status(200).render("users", {users: results});
+            });
+        }
     catch(error) {
         next(error);
     }
--- app/controllers/renderLikedPersons.js
+++ app/controllers/renderLikedPersons.js
@@ -1,38 +1,41 @@
 //Require list
+//const _array = require("lodash/array");
 const User = require("../models/User");
 
 //Display the users from the database that are a match with the user
-//This function will get the list of users that are matched with the logged in user
+//This function will get the list of users that are matched with the logged in 
+
 async function renderLikedPersons(req, res, next) {
     const {currentPerson} = req.query;
 
     //Gets all the users and their info from the database
     try{
         const users = await User.find();
-        //if the user wants to unmatch a person
-        const likeIds = users[0].likedpersons;
-        //TODO: Add user session to users[0]
-        users[0].likedpersons = likeIds.filter(id => id != currentPerson);
+        const likedObjects = [];
+        if (currentPerson) {
+            const filteredLikedPersons = users[0].likedpersons.filter(likedperson => likedperson != currentPerson);
+        users[0].likedpersons = filteredLikedPersons;
         users[0].save();
-        const likedPersons = [];
- 
-        //Gets all the users from the database
-        for (let i = 0; i < likeIds.length; i++) {
-            likedPersons.push(User.findById(likeIds[i], (err, res) => {
+        }
+
+        //Maak array aan, verwijder het object uit de array en sla het op
+        for (let i = 0; i < users[0].likedpersons.length; i++) {
+            likedObjects.push(User.findById(users[0].likedpersons[i], (err, res) => {
                 if (err) {
                     next(err);
                 } else {
                     return res;
                 }
             }));
+           
+            //likedObjects.save();
         }
 
-        // When the for-loop is finished the system will show the matches
-        await Promise.all(likedPersons)
-            .then(results => {
-                res.status(200).render("matches", {likedpersons: results});
+        await Promise.all(likedObjects)
+            .then(results =>  {
+                console.log(results);
+                res.status(200).render("matches", { likedpersons: results });
             });
-        // Saves the info about the user and sends it to matches.ejs
     }
     catch(error) {
         next(error);
