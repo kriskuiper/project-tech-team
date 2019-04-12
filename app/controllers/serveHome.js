49502e8de6d49bc3ccc433c90d9365e6c399f939
Kris <kriskuiper74@gmail.com>
2019-04-12 10:28:57 +0200

    Fix login issue

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -4,13 +4,7 @@ function serveHome(req, res) {
   if (!req.session.user) {
     res.status(200).sendFile(path.join(__dirname, "../static/pages/index.html"));
   } else {
-    res.redirect("/users");
-    const beerResults = "";
-      res.status(200).render("home", {
-        user: req.session.user,
-        beerResults : beerResults
-      });
-    }
+    res.redirect("/my-profile");
   }
 
 module.exports = serveHome;
