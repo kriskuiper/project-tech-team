a5bb7699035f7bd4e0e1f1838c747368ffc8d226
Kris <kriskuiper74@gmail.com>
2019-04-12 10:32:15 +0200

    Fix beers issue

--- app/controllers/serveHome.js
+++ app/controllers/serveHome.js
@@ -2,9 +2,12 @@ const path = require("path");
 
 function serveHome(req, res) {
   if (!req.session.user) {
-    res.status(200).sendFile(path.join(__dirname, "../static/pages/index.html"));
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, "../static/pages/index.html"));
   } else {
     res.redirect("/my-profile");
   }
+}
 
 module.exports = serveHome;
