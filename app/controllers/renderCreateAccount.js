86e50ab1045ff549983fa080ca0fc182314a26fc
Rick <rickmichiel@icloud.com>
2019-06-06 13:48:43 +0200

    Add styling for login and home

--- app/controllers/renderCreateAccount.js
+++ app/controllers/renderCreateAccount.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 function renderCreateAccount(req, res) {
-  const authLink = 'https://untappd.com/oauth/authenticate/?client_id=' + process.env.CLIENTID + '&response_type=code&redirect_url=' + process.env.REDIRECT_URL;
+  const authLink = "https://untappd.com/oauth/authenticate/?client_id=" + process.env.CLIENTID + "&response_type=code&redirect_url=" + process.env.REDIRECT_URL;
 
   res.status(200).render("create-account", {
-    authLink: authLink
+    authLink: authLink,
+    users: null,
   });
 }
 
--- app/controllers/renderLogin.js
+++ app/controllers/renderLogin.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 function renderLogin(req, res, ) {
-  const authLink = 'https://untappd.com/oauth/authenticate/?client_id=' + process.env.CLIENTID + '&response_type=code&redirect_url=' + process.env.REDIRECT_URL;
+  const authLink = "https://untappd.com/oauth/authenticate/?client_id=" + process.env.CLIENTID + "&response_type=code&redirect_url=" + process.env.REDIRECT_URL;
 
   res.status(200).render("login", {
     error: false,
-    authLink: authLink
+    authLink: authLink,
+    users: null,
   });
 }
 
