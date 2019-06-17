866cf417eafbf2cb0e8a2c70e96b2b195543b376
Aron Smit <aronsmit@hotmail.com>
2019-06-17 12:46:16 +0200

    Code cleanup

--- app/controllers/renderBars.js
+++ app/controllers/renderBars.js
@@ -1,7 +1,9 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-let {MongoClient} = require("mongodb");
+let {
+    MongoClient
+} = require("mongodb");
 let url = process.env.MONGODB_URI;
 
 
@@ -12,9 +14,11 @@ const googleApiKey = process.env.GOOGLE_API_KEY;
 
 async function renderBars(req, res) {
     //Get location of the user, to search nearby bars with google places api.
-    const { location } = req.cookies;
+    const {
+        location
+    } = req.cookies;
     const geoLocationArr = location.split("-");
-    const [ lat, long ] = geoLocationArr;
+    const [lat, long] = geoLocationArr;
 
     searchBars(lat, long);
 
@@ -26,7 +30,10 @@ async function renderBars(req, res) {
 
     await Promise.all(barImages)
         .then(images => {
-            res.status(200).render("bars", { barLocations: barLocations, barImages: images});
+            res.status(200).render("bars", {
+                barLocations: barLocations,
+                barImages: images
+            });
         });
 }
 
@@ -34,30 +41,30 @@ async function searchBars(latitude, longitude, radius = 500) {
     const response = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=bar&key=${googleApiKey}`);
     const places = await response.json();
     let noGoogle = places.error_message;
-    
-    if (noGoogle == "The provided API key is invalid.") {
-    MongoClient.connect(url, (err, db) => {
-        if (err) throw err;
-        let dbo = db.db("project-team");
-       
-            dbo.collection("bars").find({}).toArray((err, bar) => {
-          if (err) throw err;
-          barLocations = [];
-          for (let i = 0; i < bar.length; i++) {
-              barLocations.push({"name": bar[i].barname,
-              "street": bar[i].street,
-              "vicinity": bar[i].city,
-              "description": bar[i].description
-            });
 
-          }
-          
-          db.close();
+    if (noGoogle == "The provided API key is invalid.") {
+        MongoClient.connect(url, (err, db) => {
+            if (err) throw err;
+            let dbo = db.db("project-team");
+
+            dbo.collection("bars").find({}).toArray((err, bar) => {
+                if (err) throw err;
+                barLocations = [];
+                for (let i = 0; i < bar.length; i++) {
+                    barLocations.push({
+                        "name": bar[i].barname,
+                        "street": bar[i].street,
+                        "vicinity": bar[i].city,
+                        "description": bar[i].description
+                    });
+
+                }
+
+                db.close();
+            });
         });
-      });
-    }
-    else{
-    return places;
+    } else {
+        return places;
     }
 }
 
