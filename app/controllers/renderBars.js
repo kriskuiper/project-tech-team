4985e8456edc3ea365ff6213d93352b78de3b584
Aron Smit <aronsmit@hotmail.com>
2019-06-21 15:03:18 +0200

    get rid of console.log

--- app/controllers/renderBars.js
+++ app/controllers/renderBars.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 let {
@@ -13,31 +12,28 @@ let barLocations = [];
 
 
 async function searchBars(req, res) {
-        MongoClient.connect(url, (err, db) => {
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        let dbo = db.db("project-team");
+
+        dbo.collection("bars").find({}).toArray((err, bar) => {
             if (err) throw err;
-            let dbo = db.db("project-team");
-
-            dbo.collection("bars").find({}).toArray((err, bar) => {
-                if (err) throw err;
-                barLocations = [];
-                for (let i = 0; i < bar.length; i++) {
-                    barLocations.push({
-                        "name": bar[i].barname,
-                        "street": bar[i].street,
-                        "vicinity": bar[i].city,
-                        "description": bar[i].description,
-                        "imgUrl": bar[i].image
-                    });
-console.log(barLocations[i].imgUrl);
-
-                }
-
-                db.close();
-            });
-        });    
-        res.status(200).render("bars", {
-            barLocations: barLocations,
+            barLocations = [];
+            for (let i = 0; i < bar.length; i++) {
+                barLocations.push({
+                    "name": bar[i].barname,
+                    "street": bar[i].street,
+                    "vicinity": bar[i].city,
+                    "description": bar[i].description,
+                    "imgUrl": bar[i].image
+                });
+            }
+            db.close();
         });
+    });
+    res.status(200).render("bars", {
+        barLocations: barLocations,
+    });
 }
 
 module.exports = searchBars;
