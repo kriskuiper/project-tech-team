44d6696780afeed54a378ef46ae805b6e540c39b
Kris <kriskuiper74@gmail.com>
2019-06-22 17:54:04 +0200

    feat(renderBars): add pagination

--- app/controllers/renderBars.js
+++ app/controllers/renderBars.js
@@ -4,8 +4,28 @@ const Bar = require("../models/Bar");
 
 async function renderBars(req, res) {
     const jsEnabled = req.cookies.js_enabled;
+    let { limit, page } = req.query;
+    
+    const [bars, barsCount] = await Promise.all([
+        Bar.find()
+            /* Limit is not a number from itself? ¯\_(ツ)_/¯ */
+           .limit(Number(limit))
+           .skip((page-1) * limit)
+           .lean()
+           .exec(),
+        Bar.countDocuments()
+    ]);
 
-    res.status(400).render("bars", { bars: bars });
+    let pageCount = Math.ceil(barsCount / limit);
+    if (pageCount === Infinity || pageCount === 1) pageCount = 0;
+
+    res.status(400).render("bars", { 
+        bars: bars,
+        pages: paginate.getArrayPages(req)(4, pageCount, page),
+        pageCount: pageCount,
+        paginate: paginate,
+        jsEnabled: jsEnabled
+    });
 }
 
 module.exports = renderBars;
