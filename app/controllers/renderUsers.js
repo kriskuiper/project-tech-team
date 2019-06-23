8367fc8fde3ccf88ca2f6a013bfa0b0d954a7119
Chazzers <chazz@henryhandsome.nl>
2019-06-23 17:17:48 +0200

    gg

--- app/controllers/renderUsers.js
+++ app/controllers/renderUsers.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const convertToObject = require("../helpers/convertToObject");
 
 async function renderUsers(req, res, next) {
-    const { personid, min, max, gender } = req.query;
+    let { personid, min, max, gender } = req.query;
     const notLikedUsers = [];
-    const jsEnabled = req.cookies.js_enabled;
+    const userImages = [];
 
     try {
         const loggedInUser = await User.findOne({
@@ -26,20 +26,48 @@ async function renderUsers(req, res, next) {
         convertToObject(extractIds, notLikedUsers);
 
         const promisedUsers = await Promise.all(notLikedUsers);
-        const filteredUsers = promisedUsers.filter(user => user.age >= min && user.age <= max && user.gender === gender);
 
-        if (!min && !max && !gender) {
-            res.status(200).render("users", { 
-                users: promisedUsers,
-                jsEnabled: jsEnabled 
-            });
-        } else {
-            res.status(200).render("users", { 
-                users: filteredUsers,
-                jsEnabled: jsEnabled
-            });
+        const age = await function(value) {
+            value = [];
+            for (min; min <= max; min++) {
+                value.push(min);
+            }
+            return value;
+        };
+
+        let filters = {};
+
+        if(gender !== undefined) {
+            filters.gender = [gender];
         }
 
+        const ageRange = age();
+
+        if (ageRange.length >= 2) {
+            filters.age = ageRange;
+        }
+
+            const multiFilter = await function(array, filters) {
+                const filterKeys = Object.keys(filters);
+                // filters all elements passing the criteria
+                return array.filter(item => {
+                    // dynamically validate all filter criteria
+                    return filterKeys.every(key => {
+                        // ignores an empty filter
+                        if (!filters[key].length) {
+                            return true;
+                        }
+                        return filters[key].includes(item[key]);
+                    });
+                });
+            };
+
+            const filteredUsers = await multiFilter(promisedUsers, filters);
+
+        await Promise.all(userImages)
+            .then(userImages => {
+                    res.status(200).render("users", { users: filteredUsers, userImages: userImages });
+                });
     }
     catch(error) {
         next(error);
